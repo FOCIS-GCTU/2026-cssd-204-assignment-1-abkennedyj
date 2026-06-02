230896aeae87f8f137997bb3dbf7374967051f20
Josiah Kennedy <189350084+abkennedyj@users.noreply.github.com>
2026-06-02 17:07:14 +0000

    assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Josiah Abedu-Kennedy
+# STUDENT ID: 2425403734
+# Email: 2425403734@live.gctu.edu.gh
+# Grader: Emma
+#
+# On my honor, Josiah Abedu-Kennedy, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Code goes here - Print my initials
 
+    print()
+    print("...JAK")
+    print()
+
+    print("...JJJJJJJJJJJJ........AAAAAAAAAAAA........KKK......KKK.....")
+    print("...........JJJJ........AAAAAAAAAAAA........KKK.....KKK......")
+    print("...........JJJJ........AAA......AAA........KKK....KKK.......")
+    print("...........JJJJ........AAA......AAA........KKK...KKK........")
+    print("...........JJJJ........AAAAAAAAAAAA........KKKKKKK..........")
+    print("...........JJJJ........AAAAAAAAAAAA........KKKKKKK..........")
+    print("...........JJJJ........AAA......AAA........KKK...KKK........")
+    print("...JJJJ....JJJJ........AAA......AAA........KKK....KKK.......")
+    print("...JJJJJJJJJJJJ...**...AAA......AAA...**...KKK.....KKK....**")
+    print("....JJJJJJJJJJ....**...AAA......AAA...**...KKK......KKK...**")
+
+    print()
 
 
 main()
